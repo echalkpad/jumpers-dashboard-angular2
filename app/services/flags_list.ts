64980a90410cd33986f8abbc7e05f9b1cd2fe453
zsiva <m.carrizo.ibarra@gmail.com>
2016-03-29 23:28:37 +0200

    add flags components

---
+++ app/services/flags_list.ts
@@ -0,0 +1,457 @@
+export class FlagsListService {
+  flags:any[] = [
+      {
+        "class": "flags-ARGE0001 sprite_flags",
+        "picture": "ARGE0001.GIF",
+        "name": "Argentina"
+      },
+      {
+        "class": "flags-ARUB0001 sprite_flags",
+        "picture": "ARUB0001.GIF",
+        "name": "Aruba"
+      },
+      {
+        "class": "flags-ASTL0001 sprite_flags",
+        "picture": "ASTL0001.GIF",
+        "name": "Australia"
+      },
+      {
+        "class": "flags-AUST0001 sprite_flags",
+        "picture": "AUST0001.GIF",
+        "name": "Austria"
+      },
+      {
+        "class": "flags-BLRU0001 sprite_flags",
+        "picture": "BLRU0001.GIF",
+        "name": "Belarus"
+      },
+      {
+        "class": "flags-BELG0001 sprite_flags",
+        "picture": "BELG0001.GIF",
+        "name": "Belgium"
+      },
+      {
+        "class": "flags-BLZE0001 sprite_flags",
+        "picture": "BLZE0001.GIF",
+        "name": "Belize"
+      },
+      {
+        "class": "flags-BLVA0001 sprite_flags",
+        "picture": "BLVA0001.GIF",
+        "name": "Bolivia"
+      },
+      {
+        "class": "flags-BOHE0001 sprite_flags",
+        "picture": "BOHE0001.GIF",
+        "name": "Bosnia Herzegovina"
+      },
+      {
+        "class": "flags-BRAZ0001 sprite_flags",
+        "picture": "BRAZ0001.GIF",
+        "name": "Brasil"
+      },
+      {
+        "class": "flags-BULG0001 sprite_flags",
+        "picture": "BULG0001.GIF",
+        "name": "Bulgaria"
+      },
+      {
+        "class": "flags-CAVE1 sprite_flags",
+        "picture": "CAVE1.GIF",
+        "name": "Cape Verde"
+      },
+      {
+        "class": "flags-CAMB0001 sprite_flags",
+        "picture": "CAMB0001.GIF",
+        "name": "Cambodia"
+      },
+      {
+        "class": "flags-CAME0001 sprite_flags",
+        "picture": "CAME0001.GIF",
+        "name": "Cameroon"
+      },
+      {
+        "class": "flags-CANA0001 sprite_flags",
+        "picture": "CANA0001.GIF",
+        "name": "Bulgaria"
+      },
+      {
+        "class": "flags-CHIL0001 sprite_flags",
+        "picture": "CHIL0001.GIF",
+        "name": "Chile"
+      },
+      {
+        "class": "flags-CHIN0001 sprite_flags",
+        "picture": "CHIN0001.GIF",
+        "name": "China"
+      },
+      {
+        "class": "flags-CLMB0001 sprite_flags",
+        "picture": "CLMB0001.GIF",
+        "name": "Colombia"
+      },
+      {
+        "class": "flags-CORC0001 sprite_flags",
+        "picture": "CORC0001.GIF",
+        "name": "Costa Rica"
+      },
+      {
+        "class": "flags-CROA001 sprite_flags",
+        "picture": "CROA0001.GIF",
+        "name": "Croatia"
+      },
+      {
+        "class": "flags-CUBA0001 sprite_flags",
+        "picture": "CUBA0001.GIF",
+        "name": "Cuba"
+      },
+      {
+        "class": "flags-CYPR0001 sprite_flags",
+        "picture": "CYPR0001.GIF",
+        "name": "Cyprus"
+      },
+      {
+        "class": "flags-CZEC0001 sprite_flags",
+        "picture": "CZEC0001.GIF",
+        "name": "Czech Republic"
+      },
+      {
+        "class": "flags-DENM0001 sprite_flags",
+        "picture": "DENM0001.GIF",
+        "name": "Denmark"
+      },
+      {
+        "class": "flags-ECUA0001 sprite_flags",
+        "picture": "ECUA0001.GIF",
+        "name": "Ecuador"
+      },
+      {
+        "class": "flags-EGYP0001 sprite_flags",
+        "picture": "EGYP0001.GIF",
+        "name": "Egypt"
+      },
+      {
+        "class": "flags-FINL0001 sprite_flags",
+        "picture": "FINL0001.GIF",
+        "name": "Finland"
+      },
+      {
+        "class": "flags-FRAN0001 sprite_flags",
+        "picture": "FRAN0001.GIF",
+        "name": "France"
+      },
+      {
+        "class": "flags-GERM0001 sprite_flags",
+        "picture": "GERM0001.GIF",
+        "name": "Germany"
+      },
+      {
+        "class": "flags-GREC0001 sprite_flags",
+        "picture": "GREC0001.GIF",
+        "name": "Greece"
+      },
+      {
+        "class": "flags-GUAT0001 sprite_flags",
+        "picture": "GUAT0001.GIF",
+        "name": "Guatemala"
+      },
+      {
+        "class": "flags-HOND0001 sprite_flags",
+        "picture": "HOND0001.GIF",
+        "name": "Bulgaria"
+      },
+      {
+        "class": "flags-HUNG0001 sprite_flags",
+        "picture": "HUNG0001.GIF",
+        "name": "Hungary"
+      },
+      {
+        "class": "flags-ICEL0001 sprite_flags",
+        "picture": "ICEL0001.GIF",
+        "name": "Iceland"
+      },
+      {
+        "class": "flags-INDA0001 sprite_flags",
+        "picture": "INDA0001.GIF",
+        "name": "India"
+      },
+      {
+        "class": "flags-INDN0001 sprite_flags",
+        "picture": "INDN0001.GIF",
+        "name": "Indonesia"
+      },
+      {
+        "class": "flags-IREL0001 sprite_flags",
+        "picture": "IREL0001.GIF",
+        "name": "Ireland"
+      },
+      {
+        "class": "flags-ISRA0001 sprite_flags",
+        "picture": "ISRA0001.GIF",
+        "name": "Israel"
+      },
+      {
+        "class": "flags-ITAL0001 sprite_flags",
+        "picture": "ITAL0001.GIF",
+        "name": "Italy"
+      },
+      {
+        "class": "flags-JAPA0001 sprite_flags",
+        "picture": "JAPA0001.GIF",
+        "name": "Japan"
+      },
+      {
+        "class": "flags-JORD0001 sprite_flags",
+        "picture": "JORD0001.GIF",
+        "name": "Jordan"
+      },
+      {
+        "class": "flags-KENY0001 sprite_flags",
+        "picture": "KENY0001.GIF",
+        "name": "Kenya"
+      },
+      {
+        "class": "flags-SKOR0001 sprite_flags",
+        "picture": "SKOR0001.GIF",
+        "name": "South Korea"
+      },
+      {
+        "class": "flags-KURD0001 sprite_flags",
+        "picture": "KURD0001.GIF",
+        "name": "Kurdistan"
+      },
+      {
+        "class": "flags-KYRG0001 sprite_flags",
+        "picture": "KYRG0001.GIF",
+        "name": "Kyrgyzstan"
+      },
+      {
+        "class": "flags-LATV0001 sprite_flags",
+        "picture": "LATV0001.GIF",
+        "name": "Latvia"
+      },
+      {
+        "class": "flags-LIEC0001 sprite_flags",
+        "picture": "LIEC0001.GIF",
+        "name": "Liechtenstein"
+      },
+      {
+        "class": "flags-MACE0001 sprite_flags",
+        "picture": "MACE0001.GIF",
+        "name": "Macedonia"
+      },
+      {
+        "class": "flags-MALS0001 sprite_flags",
+        "picture": "MALS0001.GIF",
+        "name": "Malaysia"
+      },
+      {
+        "class": "flags-MALD0001 sprite_flags",
+        "picture": "MALD0001.GIF",
+        "name": "Maldives"
+      },
+      {
+        "class": "flags-MALT0001 sprite_flags",
+        "picture": "MALT0001.GIF",
+        "name": "Malta"
+      },
+      {
+        "class": "flags-MEXC0001 sprite_flags",
+        "picture": "MEXC0001.GIF",
+        "name": "Mexico"
+      },
+      {
+        "class": "flags-MOLD0001 sprite_flags",
+        "picture": "MOLD0001.GIF",
+        "name": "Moldavia"
+      },
+      {
+        "class": "flags-MORC0001 sprite_flags",
+        "picture": "MORC0001.GIF",
+        "name": "Morocco"
+      },
+      {
+        "class": "flags-MYAN0001 sprite_flags",
+        "picture": "MYAN0001.GIF",
+        "name": "Myanmar"
+      },
+      {
+        "class": "flags-NAMB0001 sprite_flags",
+        "picture": "NAMB0001.GIF",
+        "name": "Namibia"
+      },
+      {
+        "class": "flags-NEPA0001 sprite_flags",
+        "picture": "NEPA0001.GIF",
+        "name": "Nepal"
+      },
+      {
+        "class": "flags-NWZE0001 sprite_flags",
+        "picture": "NWZE0001.GIF",
+        "name": "New Zealand"
+      },
+      {
+        "class": "flags-NICA0001 sprite_flags",
+        "picture": "NICA0001.GIF",
+        "name": "Nicaragua"
+      },
+      {
+        "class": "flags-NORW0001 sprite_flags",
+        "picture": "NORW0001.GIF",
+        "name": "Norway"
+      },
+      {
+        "class": "flags-PALE0001 sprite_flags",
+        "picture": "PALE0001.GIF",
+        "name": "Palestina"
+      },
+      {
+        "class": "flags-PANA0001 sprite_flags",
+        "picture": "PANA0001.GIF",
+        "name": "Panama"
+      },
+      {
+        "class": "flags-PARA0001 sprite_flags",
+        "picture": "PARA0001.GIF",
+        "name": "Paraguay"
+      },
+      {
+        "class": "flags-PERU0001 sprite_flags",
+        "picture": "PERU0001.GIF",
+        "name": "Peru"
+      },
+      {
+        "class": "flags-PHIL0001 sprite_flags",
+        "picture": "PHIL0001.GIF",
+        "name": "Philippines"
+      },
+      {
+        "class": "flags-POLA0001 sprite_flags",
+        "picture": "POLA0001.GIF",
+        "name": "Poland"
+      },
+      {
+        "class": "flags-PORT0001 sprite_flags",
+        "picture": "PORT0001.GIF",
+        "name": "Portugal"
+      },
+      {
+        "class": "flags-PURC0001 sprite_flags",
+        "picture": "PURC0001.GIF",
+        "name": "Puerto Rico"
+      },
+      {
+        "class": "flags-DORE0002 sprite_flags",
+        "picture": "DORE0002.GIF",
+        "name": "Republica Dominicana"
+      },
+      {
+        "class": "flags-RMNA0001 sprite_flags",
+        "picture": "RMNA0001.GIF",
+        "name": "Romania"
+      },
+      {
+        "class": "flags-RUSS0001 sprite_flags",
+        "picture": "RUSS0001.GIF",
+        "name": "Russia"
+      },
+      {
+        "class": "flags-SERB0001 sprite_flags",
+        "picture": "SERB0001.GIF",
+        "name": "Serbia"
+      },
+      {
+        "class": "flags-SVKA0001 sprite_flags",
+        "picture": "SVKA0001.GIF",
+        "name": "Slovakia"
+      },
+      {
+        "class": "flags-SLVA0001 sprite_flags",
+        "picture": "SLVA0001.GIF",
+        "name": "Slovenia"
+      },
+      {
+        "class": "flags-SOAF0001 sprite_flags",
+        "picture": "SOAF0001.GIF",
+        "name": "South Africa"
+      },
+      {
+        "class": "flags-SPAN0001 sprite_flags",
+        "picture": "SPAN0001.GIF",
+        "name": "Spain"
+      },
+      {
+        "class": "flags-SRLA0001 sprite_flags",
+        "picture": "SRLA0001.GIF",
+        "name": "Sri Lanka"
+      },
+      {
+        "class": "flags-SWDN0001 sprite_flags",
+        "picture": "SWDN0001.GIF",
+        "name": "Sweden"
+      },
+      {
+        "class": "flags-SWIT0001 sprite_flags",
+        "picture": "SWIT0001.GIF",
+        "name": "Switzerland"
+      },
+      {
+        "class": "flags-TANZ0001 sprite_flags",
+        "picture": "TANZ0001.GIF",
+        "name": "Tanzania"
+      },
+      {
+        "class": "flags-NETH0001 sprite_flags",
+        "picture": "NETH0001.GIF",
+        "name": "The Netherlands"
+      },
+      {
+        "class": "flags-TRTB0001 sprite_flags",
+        "picture": "TRTB0001.GIF",
+        "name": "Trinidad y Tobago"
+      },
+      {
+        "class": "flags-TURK0001 sprite_flags",
+        "picture": "TURK0001.GIF",
+        "name": "Turkey"
+      },
+      {
+        "class": "flags-UNKG0001 sprite_flags",
+        "picture": "UNKG0001.GIF",
+        "name": "United Kingdom"
+      },
+      {
+        "class": "flags-UKRN0001 sprite_flags",
+        "picture": "UKRN0001.GIF",
+        "name": "Ukraine"
+      },
+      {
+        "class": "flags-URGY0001 sprite_flags",
+        "picture": "URGY0001.GIF",
+        "name": "Uruguay"
+      },
+      {
+        "class": "flags-UNST0001 sprite_flags",
+        "picture": "UNST0001.GIF",
+        "name": "United States"
+      },
+      {
+        "class": "flags-VACY0001 sprite_flags",
+        "picture": "VACY0001.GIF",
+        "name": "Vatican City"
+      },
+      {
+        "class": "flags-VIET0001 sprite_flags",
+        "picture": "VIET0001.GIF",
+        "name": "Vietnam"
+      }
+  ];
+
+  add(value:any):void {
+    this.flags.push(value);
+  }
+
+  all():any[] {
+    return this.flags;
+  }
+}
